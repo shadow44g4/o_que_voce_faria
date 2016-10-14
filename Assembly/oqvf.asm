1576c839fb8be38dd0f56ed9d7f6e7dd8cc51980
Gabriel Martin <gabriel.martin@outlook.com>
2016-10-14 14:23:33 -0300

    Assembly update

--- Assembly/oqvf.asm
+++ Assembly/oqvf.asm
@@ -7,6 +7,9 @@ op1: .asciiz "(1) - INICIAR JOGO\n"
 op2: "(2) - CREDITOS\n"
 op3: "(3) - SAIR\n"
 resp: "Opção: " #Local aonde o usuario vai colocar a resposta(pode ser usado mais de uma vez)
+texto1: "case 1"
+texto2: "case 2"
+
 .text ## nem sei o que faz mas eu boto mesmo assim
 
 main: ## Inicio
@@ -31,8 +34,16 @@ main: ## Inicio
 	la $a0, resp
 	syscall
 	
+	
+	
+	
+	
 jal le_inteiro_do_teclado  # chama função para ler
 la  $t7, 0($v0)		   # carrega o inteiro lido em $t7
+
+beq $t7, 1, case1
+beq $t7, 2, case2
+
 li $v0, 10 # finaliza a parada
 syscall
 le_inteiro_do_teclado:
@@ -40,3 +51,17 @@ le_inteiro_do_teclado:
 		syscall		# executa a chamada do SO para ler
 		jr $ra		# volta para o lugar de onde foi chamado (no caso, jal le_inteiro_do_teclado)
 
+case1:	li $v0, 4
+	la $a0, texto1
+	syscall 
+	jr $ra
+	
+case2:	li $v0, 4
+	la $a0, texto2
+	syscall 
+	jr $ra
+	
+cas3:	
+	syscall 
+	jr $ra
+	
